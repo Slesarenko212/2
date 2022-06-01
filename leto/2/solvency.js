c07c4609ebba3711dc56c5b3b173f1c03d1a39f9
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-01 19:21:31 +0300

    Create solvency.js

---
+++ leto/2/solvency.js
@@ -0,0 +1,7 @@
+function func(){
+          var income = Number(document.getElementById("income").value);
+          var duty = Number(document.getElementById("duty").value);
+          var solvency = income * 0.8 - duty;
+
+          document.getElementById("solvency").innerHTML = solvency;
+          }
