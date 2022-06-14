8416a2b02d2be30920115e57e9f7978213916241
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-14 19:43:33 +0300

    Update solvency.js

--- leto/2/solvency.js
+++ leto/2/solvency.js
@@ -1,14 +1,10 @@
 window.addEventListener('DOMContentLoaded', function (event) {
-    let regex = /^\s*\d+\s*$/;
     let b = document.getElementById("mybutt");
     b.addEventListener("click", function (event) {
-        //let regex = /[./n]*/
-        let income = document.getElementById("income");
-        let duty = document.getElementById("duty");
-        let solvency = document.getElementById("solvency");
-        if (regex.test(income.value) && regex.test(duty.value))
-            solvency.innerHTML = income.value * 0.8 - duty.value - 14000;
-        else
-            alert("Invalid input!");
+        let income_var = document.getElementById("income");
+        let duty_var = document.getElementById("duty");
+        let solvency_var = document.getElementById("solvency");
+        
+        solvency_var.innerHTML = income_var.value * 0.8 - duty_var.value - 14000;
         });
     });
