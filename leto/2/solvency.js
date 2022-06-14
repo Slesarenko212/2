db21b5380a3434991b9b3b7b6cecc8ef438b04fe
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-14 19:32:07 +0300

    Update solvency.js

--- leto/2/solvency.js
+++ leto/2/solvency.js
@@ -1,7 +1,14 @@
-function Solvency(){
-          var income = Number(document.getElementById("income").value);
-          var duty = Number(document.getElementById("duty").value);
-          var solvency = income * 0.8 - duty - 14000;
-
-          document.getElementById("solvency").innerHTML = solvency;
-          }
+window.addEventListener('DOMContentLoaded', function (event) {
+    let regex = /^\s*\d+\s*$/;
+    let b = document.getElementById("mybutt");
+    b.addEventListener("click", function (event) {
+        //let regex = /[./n]*/
+        let income = document.getElementById("income");
+        let duty = document.getElementById("duty");
+        let solvency = document.getElementById("solvency");
+        if (regex.test(income.value) && regex.test(duty.value))
+            solvency.innerHTML = income.value * 0.8 - duty.value - 14000;
+        else
+            alert("Invalid input!");
+        });
+    });
