12a0e41737eca014e9454106de63fe7a4ad76306
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-14 20:02:08 +0300

    Update solvency.js

--- leto/2/solvency.js
+++ leto/2/solvency.js
@@ -5,7 +5,7 @@ window.addEventListener('DOMContentLoaded', function (event) {
         let duty_var = document.getElementById("duty");
         let solvency_var = document.getElementById("solvency");
         
-        if ((income_var.value != 0)||(duty_var.value != 0))
+        if ((income_var.value != 0)&&(duty_var.value != 0))
             solvency_var.innerHTML = income_var.value * 0.8 - duty_var.value - 14000;
         else
             alert("Введите данные!");
