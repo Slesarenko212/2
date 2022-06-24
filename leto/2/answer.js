b55c641977ad3a324306cdd358d71ecfe1a38494
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-25 00:44:52 +0300

    Create answer.js

---
+++ leto/2/answer.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function (event) {
+    let b = document.getElementById("mybutt");
+    b.addEventListener("click", function (event) {
+        let answer_var = document.getElementById("answer");
+        
+        if (answer.value != 0)
+            alert("Ответ отправлен")
+        else
+            alert("Введите текст ответа!");
+        });
+    });
