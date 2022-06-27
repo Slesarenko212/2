8fd647d8751dd282c7e49076688cb08d5f698e58
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-27 18:23:19 +0300

    Update teh_support.js

--- leto/2/teh_support.js
+++ leto/2/teh_support.js
@@ -4,9 +4,39 @@ window.addEventListener('DOMContentLoaded', function (event) {
         let topic_var = document.getElementById("topic");
         let description_var = document.getElementById("description");
         
-        if ((topic.value != 0)&&(description_var.value != 0))
-            alert("Успешно!")
-        else
-            alert("Введите данные!");
+        if (topic_var.value == 0)
+            showError('topic', 'Заполните поле!');
+        if (description_var.value == 0)
+            chsError('description', 'Заполните поле!');
         });
     });
+
+
+function showError(field, errorMessage) {
+	var errorSpan = document.createElement("span");
+	var errorMessage = document.createTextNode(errorMessage);
+
+	errorSpan.appendChild(errorMessage);
+	errorSpan.className = "errorMsg";
+
+	var fieldLabel = document.getElementById(field).previousSibling;
+	while (fieldLabel.nodeName.toLowerCase() != "label") {
+		fieldLabel = fieldLabel.previousSibling;
+	}
+	fieldLabel.appendChild(errorSpan);
+}
+
+
+function chsError(field, chserrorMessage) {
+	var errorSpan = document.createElement("span");
+	var chserrorMessage = document.createTextNode(chserrorMessage);
+
+	errorSpan.appendChild(chserrorMessage);
+	errorSpan.className = "chserrorMsg";
+
+	var fieldLabel = document.getElementById(field).previousSibling;
+	while (fieldLabel.nodeName.toLowerCase() != "label") {
+		fieldLabel = fieldLabel.previousSibling;
+	}
+	fieldLabel.appendChild(errorSpan);
+}
