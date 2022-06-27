80027199ce8e60896c308c228823d642dd175ce3
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-27 22:57:39 +0300

    Update solvency.js

--- leto/2/solvency.js
+++ leto/2/solvency.js
@@ -6,8 +6,43 @@ window.addEventListener('DOMContentLoaded', function (event) {
         let solvency_var = document.getElementById("solvency");
         
         if ((income_var.value != 0)&&(duty_var.value != 0))
-            solvency_var.innerHTML = Math.ceil(income_var.value * 0.8 - duty_var.value - 14000);
+            solvency_var.innerHTML = Math.ceil(income_var.value * 0.8 - duty_var.value - 14000)
         else
-            alert("Введите данные!");
+        {
+            if (income_var.value == 0)
+                showError('income', 'Заполните поле!');
+            if (duty_var.value == 0)
+                showError('duty', 'Заполните поле!');
+        }
         });
     });
+
+
+function showError(field, errorMessage) {
+	var errorSpan = document.createElement("span");
+	var errorMessage = document.createTextNode(errorMessage);
+
+	errorSpan.appendChild(errorMessage);
+	errorSpan.className = "errorMsg";
+
+	var fieldLabel = document.getElementById(field).previousSibling;
+	while (fieldLabel.nodeName.toLowerCase() != "label") {
+		fieldLabel = fieldLabel.previousSibling;
+	}
+	fieldLabel.appendChild(errorSpan);
+}
+
+
+function chsError(field, chserrorMessage) {
+	var errorSpan = document.createElement("span");
+	var chserrorMessage = document.createTextNode(chserrorMessage);
+
+	errorSpan.appendChild(chserrorMessage);
+	errorSpan.className = "chserrorMsg";
+
+	var fieldLabel = document.getElementById(field).previousSibling;
+	while (fieldLabel.nodeName.toLowerCase() != "label") {
+		fieldLabel = fieldLabel.previousSibling;
+	}
+	fieldLabel.appendChild(errorSpan);
+}
