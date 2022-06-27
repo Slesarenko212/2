5cb7bb38475110ea7496fc74d8aa87f9d6c725da
Slesarenko212 <90548202+Slesarenko212@users.noreply.github.com>
2022-06-27 16:51:51 +0300

    Update black_list.js

--- leto/2/black_list.js
+++ leto/2/black_list.js
@@ -4,22 +4,12 @@ window.addEventListener('DOMContentLoaded', function (event) {
         let inn_var = document.getElementById("inn");
         let chs_var = document.getElementById("chs");
         
-        if ((inn_var.value != 0)&&(chs_var.value != 0)&&(inn_var.value.length == 12))
-            alert("Успешно!")
-        else
-            if ((inn_var.value != 0)&&(chs_var.value != 0))
-                showError('inn', 'Проверьте ИНН!')
-            else
-		if (inn_var.value != 0)
-	    		showError('chs', 'Заполните поле!')
-	    	else
-			if (chs_var.value != 0)
-				showError('inn', 'Заполните поле!')
-	    		else
-			{
-				showError('inn', 'Заполните поле!');
-				showError('chs', 'Заполните поле!');
-			}
+        if (inn_var.value == 0)
+		showError('inn', 'Заполните поле!');
+        if (chs_var.value == 0)
+                showError('chs', 'Заполните поле!');
+        if ((inn_var.value != 0)&&(inn_var.value.length != 12))
+		showError('inn', 'Проверьте ИНН!');
         });
     });
 
